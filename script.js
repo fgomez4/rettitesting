cbfe3dc2f20aecce77b1efe62e819d5761974793
fgomez4@twu.edu <98373798+fabikaro@users.noreply.github.com>
2023-11-04 17:32:37 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,10 @@
+function openContactPage() {
+    var name = document.getElementById('name').value;
+    var phone = document.getElementById('phone').value;
+    var income = document.getElementById('income').value;
+    var creditScore = document.getElementById('creditScore').value;
+
+    // You can use the collected data to customize the contact page or perform other actions.
+    // For now, let's simply alert the user with the information.
+    alert(`Thank you, ${name}!\n\nContact Details:\nPhone: ${phone}\nIncome: ${income}\nCredit Score: ${creditScore}`);
+}
