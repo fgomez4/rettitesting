8217035af68fc8b217fb283cfd66759f5ddbbac7
fgomez4@twu.edu <98373798+fabikaro@users.noreply.github.com>
2023-11-05 01:22:53 -0500

    adding dark and light mode

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ function openContactPage() {
     // For now, let's simply alert the user with the information.
     alert(`Thank you, ${name}!\n\nContact Details:\nPhone: ${phone}\nIncome: ${income}\nCredit Score: ${creditScore}`);
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+    const toggleSwitch = document.getElementById('theme-switch');
+
+    // Check the user's preference from localStorage
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        body.classList.add(currentTheme);
+    } else {
+        // Set the initial theme based on user's preference or default to light
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        body.classList.add(prefersDarkMode ? 'dark-mode' : 'light-mode');
+    }
+
+    // Toggle between light and dark modes
+    toggleSwitch.addEventListener('change', function () {
+        body.classList.toggle('dark-mode');
+        body.classList.toggle('light-mode');
+
+        // Save the user's preference to localStorage
+        const theme = body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
+        localStorage.setItem('theme', theme);
+    });
+});
+
